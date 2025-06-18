88154f16e382702b624cdd3e334ba6d0a4191a63
Travis <Travisang40@gmail.com>
2025-06-18 11:16:04 +0800

    update

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,7 @@
 import { env } from "@/data/env/server";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "@/db/schema";
+import { neon } from "@neondatabase/serverless";
 
-export const db = drizzle(env.AUTH_DRIZZLE_URL, { schema });
+const sql = neon(env.AUTH_DRIZZLE_URL);
+export const db = drizzle(sql, { schema });
