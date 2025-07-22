30d19a03490725e35ec9e5b8b8055935e33221e5
Travis <Travisang40@gmail.com>
2025-07-22 23:51:55 +0800

    update stripe version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,5 +2,5 @@ import { env } from "@/data/env/server";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2025-06-30.basil",
 });
